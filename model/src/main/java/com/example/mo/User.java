72cb62c9beb7255e769aae75db42750d12c17aa0
NamPhong <nguyennamphong2111@gmail.com>
2024-05-28 11:39:20 +0700

    phong

--- model/src/main/java/com/example/mo/User.java
+++ model/src/main/java/com/example/mo/User.java
@@ -1,7 +1,7 @@
-package com.example.model;
+package com.example.mo;
 
 public class User {
     public User() {
-        
+    
     }
 }
--- model/src/main/java/com/example/view/phong.java
+++ model/src/main/java/com/example/view/phong.java
@@ -3,5 +3,6 @@ package com.example.view;
 public class phong {
     public phong() {
         System.out.println("phong");
+        System.out.println("nam");
     }
 }
