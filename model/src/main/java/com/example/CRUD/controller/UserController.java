3eb93305dff13e1795f5b0cd3f8f5a5e69e63d5c
NamPhong <nguyennamphong2111@gmail.com>
2024-06-11 11:38:51 +0700

    Quan

---
+++ model/src/main/java/com/example/CRUD/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.CRUD.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Repository.UserRepository;
+import com.example.mo.Users;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+    @ModelAttribute
+    public void commonUser(Principal p, Model m) {
+        if (p != null) {
+            String email = p.getName();
+            Users user = userRepo.findByEmail(email);
+            m.addAttribute("user", user);
+        }
+    }
+}
